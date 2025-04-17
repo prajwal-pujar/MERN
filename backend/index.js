15a911e7b0fafb464a5b75fe734169b86fd8c2d2
prajwal-pujar <prajwalpujar24@gmail.com>
2025-04-17 19:08:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,16 @@ const mongoose = require('mongoose');
 const connect = require('./mongodb')
 const cors = require('cors');
 connect()
+
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://mern-6gc8-prajwal-pujars-projects.vercel.app', // Allow only this frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Allow specific headers
+}));
+
 app.use(express.json());
-app.use(cors());
+
 app.use('/auth',require('./routes/auth'))
 app.use('/mssg',require('./routes/livemssg'))
 app.use('/upload' , require('./routes/upload'))
