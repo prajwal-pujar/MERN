cc738b3839e329961b916790ac1367d1830ae843
prajwal-pujar <prajwalpujar24@gmail.com>
2025-04-17 19:01:27 +0530

    Update Login.js

--- frontend/src/componemts/Login.js
+++ frontend/src/componemts/Login.js
@@ -21,7 +21,7 @@ function Login() {
     event.preventDefault();
     setIsLoading(true); // Start loading
     try {
-      const response = await fetch('http://localhost:5000/auth/login', {
+      const response = await fetch('https://mern-zeta-nine.vercel.app/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
