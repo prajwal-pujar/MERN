de730a7d816c3d98dbc040fb0fca7e1509037f0e
prajwal-pujar <prajwalpujar24@gmail.com>
2025-05-02 09:21:13 +0530

    Update Mssg.js

--- frontend/src/componemts/Mssg.js
+++ frontend/src/componemts/Mssg.js
@@ -39,7 +39,7 @@ export default function Mssg() {
 
   return (
     <div className="chat-container">
-      {friends[tok] && (
+      {image && nam && && (
         <div className="user-info d-flex align-items-center justify-content-center mb-3">
           <img
             src={image}
