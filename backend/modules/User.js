30f7f2e076de7f64464d908ae83e872025bab45e
prajwal-pujar <prajwalpujar24@gmail.com>
2025-05-06 10:01:08 +0530

    Update User.js

--- backend/modules/User.js
+++ backend/modules/User.js
@@ -18,6 +18,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    isOnline: {
+        type: Boolean,
+        default: false
+    },
     date: {
         type: Date,
         default: Date.now
