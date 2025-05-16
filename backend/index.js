4d4c9fa652f6facf66f0813d95eaefe903ac0ff0
prajwal-pujar <prajwalpujar24@gmail.com>
2025-05-16 09:16:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,55 @@
 const express = require('express');
-const app = express();
-const port = 5000;
+const http = require('http');
+const { Server } = require('socket.io');
 const mongoose = require('mongoose');
-const connect = require('./mongodb');
 const cors = require('cors');
+const connect = require('./mongodb');
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'https://mern-6gc8-prajwal-pujars-projects.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'auth-token', 'send-token', 'rec-token'],
+  }
+});
+
+// MongoDB connection
 connect();
 
-
+// CORS middleware
 const corsOptions = {
-origin: 'https://mern-6gc8-prajwal-pujars-projects.vercel.app', 
-methods: ['GET', 'POST', 'PUT', 'DELETE'],
-allowedHeaders: ['Content-Type', 'auth-token', 'send-token', 'rec-token'],
+  origin: 'https://mern-6gc8-prajwal-pujars-projects.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'auth-token', 'send-token', 'rec-token'],
 };
-
-
 app.use(cors(corsOptions));
-app.options('\*', cors(corsOptions));
+app.options('*', cors(corsOptions));
 
-
+// Body parser
 app.use(express.json({ limit: '5mb' }));
 
+// Routes
 app.use('/auth', require('./routes/auth'));
 app.use('/mssg', require('./routes/livemssg'));
 app.use('/upload', require('./routes/upload'));
 
 app.get('/', (req, res) => {
-res.send('HelloWorld!');
+  res.send('HelloWorld!');
 });
 
-module.exports = app;
+// Socket.IO handlers
+const users = {}
+io.on('connection', (socket) => {
+    socket.on('new user joined' , (name)=>{
+      users[socket.id] = name;
+      socket.broadcast.emit('user joined' + name)
+    })
+});
 
-
+// Start server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
