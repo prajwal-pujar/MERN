e3733da68014c0cfa0da9d5b94fbd9cbacd1cf32
prajwal-pujar <prajwalpujar24@gmail.com>
2025-05-16 09:39:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,58 +1,31 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const connect = require('./mongodb');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: 'https://mern-6gc8-prajwal-pujars-projects.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'auth-token', 'send-token', 'rec-token'],
-  }
-});
+const port = 5000;
+const mongoose = require('mongoose');
+const connect = require('./mongodb');
+const cors = require('cors');
 
-// MongoDB connection
 connect();
 
-// CORS middleware
 const corsOptions = {
-  origin: 'https://mern-6gc8-prajwal-pujars-projects.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'auth-token', 'send-token', 'rec-token'],
+origin: '[https://mern-6gc8-prajwal-pujars-projects.vercel.app](https://mern-6gc8-prajwal-pujars-projects.vercel.app)',
+methods: \['GET', 'POST', 'PUT', 'DELETE'],
+allowedHeaders: \['Content-Type', 'auth-token', 'send-token', 'rec-token'],
 };
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
-// Body parser
+app.use(cors(corsOptions));
+app.options('\*', cors(corsOptions));
+
 app.use(express.json({ limit: '5mb' }));
 
-// Routes
 app.use('/auth', require('./routes/auth'));
 app.use('/mssg', require('./routes/livemssg'));
 app.use('/upload', require('./routes/upload'));
 
 app.get('/', (req, res) => {
-  res.send('HelloWorld!');
+res.send('HelloWorld!');
 });
 
-// Socket.IO handlers
-io.on('connection', (socket) => {
-  socket.on('join-room', (room) => {
-    socket.join(room);
-  });
-
-  socket.on('send-message', ({ from, to, text }) => {
-    io.to(to).emit('receive-message', { name: from, text });
-  });
-});
+module.exports = app;
 
 
-// Start server
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
